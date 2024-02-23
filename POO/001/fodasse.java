4c7e9f768420467c7cfdd18d572d3f8d338ba38f
Adriano Vale <78498215+ValeAdriano@users.noreply.github.com>
2024-02-23 19:38:56 -0300

    .

---
+++ POO/001/fodasse.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class fodasse{
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Teste123");
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Qual a forma de pagamento?\n [1] A vista -10%\n [2] Credito 1x\n [3] Credito 2x +10%");
+
+        Integer pgt = teclado.nextInt();
+        
+        System.out.println("pgt");
+
+
+    }
+}
